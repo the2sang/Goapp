5cde8cbff803932d48024e060c19f60fb6afddf6
Nico Boco <74524734+the2sang@users.noreply.github.com>
2023-03-10 22:03:56 +0900

    Create goapp.go

---
+++ goapp.go
@@ -0,0 +1,27 @@
+import (
+  "fmt"
+  "os"
+  "log"
+  "net"
+  "net/http"
+)
+func gohandler(w http.ResponseWriter, r *http.Request) {
+  name, err := os.Hostname()
+  if err != nil {
+    fmt.Printf("error: %v\n", err)
+    return
+  }
+  fmt.Fprintln(w, "Hostname: ", name)
+  
+  addr, err := net.LookupHost(name)
+  if err != nil {
+    fmt.Printf("error: %v\n", err)
+    return
+  }
+  fmt.Fprintfln(w, "IP: ", addr)
+}
+func main() {
+  fmt.Fprintln(os.Stdout, "Go!!! Go Application......")
+  http.HandleFunc("/", gohandler)
+  log.Fatal(http.ListenAndServe(":9090", nil))
+}
